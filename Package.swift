689a4ef235f48c4671cb8846490610cf0814e118
Brandon Erbschloe <berbschloe@gmail.com>
2020-06-05 11:54:13 -0500

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "LayoutPriorityUtils",
-  platforms: [
-    .iOS(.v9)
-  ],
-  products: [
-    .library(
-      name: "LayoutPriorityUtils", 
-      targets: ["LayoutPriorityUtils"]
-    ),
-  ],
-  targets: [
-    .target(
-      name: "LayoutPriorityUtils", 
-      path: "LayoutPriorityUtils"
-    ),
-    .testTarget(
-      name: "LayoutPriorityUtilsTests", 
-      dependencies: ["LayoutPriorityUtils"], 
-      path: "LayoutPriorityUtilsTests"
-    ),
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "LayoutPriorityUtils",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "LayoutPriorityUtils", targets: ["LayoutPriorityUtils"]),
+    ],
+    targets: [
+        .target(name: "LayoutPriorityUtils", path: "LayoutPriorityUtils"),
+        .testTarget(name: "LayoutPriorityUtilsTests", dependencies: ["LayoutPriorityUtils"]),
+    ]
 )
